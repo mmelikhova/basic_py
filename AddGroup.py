fd0ee69cea7e8d11b3cb62285ebd25cf6dd10f68
mmelikhova <milena.nurgaleeva@gmail.com>
2020-06-09 20:54:16 +0300

    ver.3 - Task 2

--- AddGroup.py
+++ AddGroup.py
@@ -8,36 +8,48 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from group import Group
 
 class AddGroup(unittest.TestCase):
+
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd, group):
+        # create new group
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new_mln_group")
+        wd.find_element_by_name("group_name").send_keys(group.groupname)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("hello")
+        wd.find_element_by_name("group_header").send_keys(group.header_text)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("hello")
+        wd.find_element_by_name("group_footer").send_keys(group.footer_text)
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-    
+
+    def login(self, wd, username, password):
+        # login
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_page(self, wd):
+        # open page
+        wd.get("http://localhost/addressbook/group.php")
+
     def is_element_present(self, how, what):
         try:
             self.wd.find_element(by=how, value=what)
@@ -45,10 +57,28 @@ class AddGroup(unittest.TestCase):
         return True
     
     def is_alert_present(self):
-        try: self.wd.switch_to_alert()
+        try: 
+            self.wd.switch_to_alert()
         except NoAlertPresentException as e:
             return False
         return True
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, Group(groupname="new_mln_group", header_text="hello", footer_text="hello"))
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_group(wd, Group(groupname="", header_text="", footer_text=""))
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
 
     def tearDown(self):
         self.wd.quit()
---
+++ group.py
@@ -0,0 +1,7 @@
+
+class Group:
+
+    def __init__(self, groupname, header_text, footer_text):
+        self.groupname = groupname
+        self.header_text = header_text
+        self.footer_text = footer_text
