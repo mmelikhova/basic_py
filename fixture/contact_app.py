dc68795e7b43dcf24c8e94377915d3d44de45151
mmelikhova <milena.nurgaleeva@gmail.com>
2020-06-19 00:05:57 +0300

    Task 5 - выполнена декомпозиция кода для добавления контактов

--- fixture/contact_app.py
+++ fixture/contact_app.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
+from fixture.session2 import SessionHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -7,138 +8,12 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(120)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session2 = SessionHelper(self)
+        self.contact = ContactHelper(self)
 
     def return_homepage(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
-
-    def submit(self):
-        wd = self.wd
-        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-
-    def fill_note(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys(contact.note)
-
-    def fill_homephone(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").clear()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-
-    def fill_homeaddress(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").clear()
-        wd.find_element_by_name("address2").send_keys(contact.home_address)
-
-    def fill_aday(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(contact.aday)
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(contact.amonth)
-        wd.find_element_by_name("amonth").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-
-    def fill_bday(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-
-    def fill_homepage(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-
-    def fill_mails(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contact.mail1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(contact.mail2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(contact.mail3)
-
-    def fill_phones(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_num)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mob_num)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_num)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax_num)
-
-    def fill_company_info(self, contact):
-        wd = self.wd
-        wd.find_element_by_xpath("//div[@id='content']/form/label[7]").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.company_address)
-
-    def fill_title(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contact.title_text)
-
-    def fill_name(self, contact):
-        wd = self.wd
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-
-    def add_new(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, login, password):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(login)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_page(self):
         wd = self.wd
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,13 +7,13 @@ from fixture.contact_app import Application
 
 
 @pytest.fixture
-def appr(request):
+def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 
-def test_add_contact(appr):
+def test_add_contact(app):
         c = Contact(note="note", phone2="22", home_address="vit57", homepage="mln.ru",
                           middlename="Melikhov", lastname="Melikhov", nickname="mln_mln", firstname="Lesha",
                           company_address="VO 9th line", company="T-systems",
@@ -22,22 +22,22 @@ def test_add_contact(appr):
                           bday="2", bmonth="January", byear="1996",
                           aday="1", amonth="February", ayear="2003",
                           title_text="text")
-        appr.open_page()
-        appr.login(login="admin", password="secret")
-        appr.add_new()
-        appr.fill_name(c)
-        appr.fill_title(c)
-        appr.fill_company_info(c)
-        appr.fill_phones(c)
-        appr.fill_mails(c)
-        appr.fill_homepage(c)
-        appr.fill_bday(c)
-        appr.fill_aday(c)
-        appr.fill_homeaddress(c)
-        appr.fill_homephone(c)
-        appr.fill_note(c)
-        appr.submit()
-        appr.return_homepage()
-        appr.logout()
+        app.open_page()
+        app.session2.login(login="admin", password="secret")
+        app.contact.add_new()
+        app.contact.fill_name(c)
+        app.contact.fill_title(c)
+        app.contact.fill_company_info(c)
+        app.contact.fill_phones(c)
+        app.contact.fill_mails(c)
+        app.contact.fill_homepage(c)
+        app.contact.fill_bday(c)
+        app.contact.fill_aday(c)
+        app.contact.fill_homeaddress(c)
+        app.contact.fill_homephone(c)
+        app.contact.fill_note(c)
+        app.contact.submit()
+        app.return_homepage()
+        app.session2.logout()
 
 
