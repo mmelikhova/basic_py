0de2c911e32a8ea6288a82663e83fef811efdef0
mmelikhova <milena.nurgaleeva@gmail.com>
2020-07-18 16:53:42 +0300

    Задание №15: коррективы

--- conftest.py
+++ conftest.py
@@ -1,28 +1,23 @@
 import pytest
 from fixture.application import Application
 
-fixture=None
+
+fixture = None
 
 
 @pytest.fixture()
-def app():
+def app(request):
     global fixture
-    if fixture is None:
-        fixture=Application()
-        fixture.session.login(login="admin", password="secret")
-    else:
-        if not fixture.is_valid():
-            fixture=Application()
-            fixture.session.ensure_login(login="admin", password="secret")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application()
+    fixture.session.ensure_login(login="admin", password="secret")
     return fixture
 
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope="session", autouse=True)
 def stop(request):
-
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
-
     request.addfinalizer(fin)
     return fixture
--- fixture/application.py
+++ fixture/application.py
@@ -7,28 +7,26 @@ from fixture.contact import ContactHelper
 class Application:
 
     def __init__(self):
-        self.wd = webdriver.Firefox()
-        self.session = SessionHelper(self)
-        self.group = GroupHelper(self)
-        self.contact = ContactHelper(self)
+        self.wd=webdriver.Firefox()
+        self.session=SessionHelper(self)
+        self.group=GroupHelper(self)
+        self.contact=ContactHelper(self)
 
     def destroy(self):
         self.wd.quit()
 
     def is_valid(self):
         try:
-            self.wd.current_url("http://localhost/addressbook/")
+            self.wd.current_url()
             return True
-        except:
+        finally:
             return False
 
     def open_page(self):
-        wd = self.wd
-        if not(wd.current_url == "http://localhost/addressbook/"):
+        wd=self.wd
+        if not (wd.current_url == "http://localhost/addressbook/"):
             wd.get("http://localhost/addressbook/")
 
     def return_homepage(self):
-         wd = self.wd
-         wd.find_element_by_xpath("/html/body/div/div[3]/ul/li[1]/a").click()
-
-
+        wd=self.wd
+        wd.find_element_by_xpath("/html/body/div/div[3]/ul/li[1]/a").click()
