57fe95d56448bb1ae024f4c326f12cae2e549c74
mmelikhova <milena.nurgaleeva@gmail.com>
2020-07-20 19:33:02 +0300

    Задание №19: загрузка и чтение тестовых данных из файлов генератора(json)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,39 +7,9 @@ import random
 import string
 
 
-def random_string(prefix, maxlen):
-    symbols=string.ascii_letters + string.digits + " " * 10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-
-month_list=['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
-            'November', 'December']
-
-
-testdata= [Contact(note=random_string("note", 20),
-                                             phone2=random_string("ph2", 10),
-                                             home_address=random_string("homeadd", 20),
-                                             homepage=random_string("homepage", 20),
-                                             middlename=random_string("midn", 25), lastname=random_string("lastn", 25),
-                                             nickname=random_string("nick", 25), firstname=random_string("firstn", 25),
-                                             company_address=random_string("compadd", 35),
-                                             company=random_string("compname", 20),
-                                             home_num=random_string("homeph", 10), mob_num=random_string("mobph", 10),
-                                             work_num=random_string("workph", 10), fax_num=random_string("fax", 10),
-                                             mail1=random_string("1mail", 20), mail2=random_string("2mail", 20),
-                                             mail3=random_string("3mail", 20),
-                                             bday=str(random.randrange(0, 31, 1)), bmonth=random.choice(month_list),
-                                             byear=str(random.randrange(1900, 2020, 1)),
-                                             aday=str(random.randrange(0, 31, 1)), amonth=random.choice(month_list),
-                                             ayear=str(random.randrange(1900, 2020, 1)),
-                                             title_text=random_string("title", 10))
-                                     for i in range(3)
-                                     ]
-
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add_new()
     app.contact.fill_name(contact)
