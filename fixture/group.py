a2e0f9daeba8fffafc26e4012fa9b83d838989e5
mmelikhova <milena.nurgaleeva@gmail.com>
2020-07-24 14:52:00 +0300

    Тема 7: Драйвер базы данных, выполнение простых SQL-запросов Фикстура для взаимодействия с базой данных Загрузка информации из базы данных в виде объектов, проверяем данные, загруженные из базы данных

--- fixture/group.py
+++ fixture/group.py
@@ -53,6 +53,16 @@ class GroupHelper:
         self.return_to_group_page()
         self.group_cache=None
 
+    def delete_group_by_id(self, id):
+        wd=self.app.wd
+        self.open_gr_page()
+        # select group
+        self.select_group_by_id(id)
+        # submit delete
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
+        self.group_cache=None
+
     def select_first_group(self):
         wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
@@ -60,6 +70,11 @@ class GroupHelper:
     def select_group_by_index(self, index):
         wd = self.app.wd
         wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_group_by_id(self, id):
+        wd=self.app.wd
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
 
     def modify_group_by_index(self, new_group_data, index):
         wd = self.app.wd
