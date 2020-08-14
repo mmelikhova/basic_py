e527096a0c56868bd7a022ac6d7ad7ad1dfd9ffe
mmelikhova <milena.nurgaleeva@gmail.com>
2020-08-14 17:47:44 +0300

    Tema 10 - BDD for Groups

---
+++ bdd/group_steps.py
@@ -0,0 +1,74 @@
+from pytest_bdd import given, when, then
+from model.group import Group
+import random
+
+@given('a group list')
+def group_list(db):
+    return db.get_group_list()
+
+@given('a group with <name>, <header> and <footer>')
+def new_group(name, header, footer):
+    return Group(name=name, header=header, footer=footer)
+
+@when('I add the group to the list')
+def add_new_group(app, new_group):
+    app.group.create(new_group)
+
+@then('the new group list is equal to the old list with the added group')
+def verify_group_added(db, group_list, new_group):
+    old_groups = group_list
+    new_groups = db.get_group_list()
+    old_groups.append(new_group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
+@given('a non-empty group list')
+def non_empty_group_list(db, app):
+    if len(db.get_group_list()) == 0:
+        app.group.create(Group(name="groupname"))
+    return db.get_group_list()
+
+@given('a random group from the list')
+def random_group(non_empty_group_list):
+    return random.choice(non_empty_group_list)
+
+# del gr
+
+@when('I delete a group from the list')
+def delete_group(app, random_group):
+    app.group.delete_group_by_id(random_group.id)
+
+@then('the new group list is equal to the old list without the deleted group')
+def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) - 1 == len(new_groups)
+    old_groups.remove(random_group)
+    assert old_groups == new_groups
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
+
+# edit group
+
+@given('a random group from the list')
+def index_random_group(non_empty_group_list):
+    random_group_index = random.randrange(len(non_empty_group_list))
+    return random_group_index
+
+@given('a group with <name>, <header> and <footer>')
+def group_modify(name, header, footer):
+    return Group(name=name, header=header, footer=footer)
+
+@when('I modify the group in the list')
+def modify_group(app, non_empty_group_list, index_random_group, group_modify):
+    group_modify.id = non_empty_group_list[index_random_group].id
+    app.group.modify_group_by_id(group_modify.id, group_modify)
+
+@then('the new group list is equal to the old list with the modified group')
+def verify_group_modified(app, db, non_empty_group_list, index_random_group, group_modify, check_ui):
+    old_groups = non_empty_group_list
+    new_groups = db.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[index_random_group] = group_modify
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
--- fixture/contact.py
+++ fixture/contact.py
@@ -131,6 +131,21 @@ class ContactHelper:
         wd.find_element_by_name("nickname").send_keys(contact.nickname)
         self.contact_cache=None
 
+    def create_contact(self, contact):
+        self.add_new()
+        wd=self.app.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.company_address)
+        self.submit()
+        self.app.homepage()
+
     def add_new(self):
         wd = self.app.wd
         wd.find_element_by_link_text("add new").click()
